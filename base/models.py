6fb5cf0da4a01e0613f960ed163d749a803c61aa
Cipher <67450280+Briankurtish@users.noreply.github.com>
2022-11-13 20:57:22 +0300

    setting up the database with models

--- base/models.py
+++ base/models.py
@@ -11,3 +11,6 @@ class Room(models.Model):
     # participants =
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __self__(self):
+        return self.name
