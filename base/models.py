00ce88d17cc51dd5554e8a01bb1032d4640edf9e
Cipher <67450280+Briankurtish@users.noreply.github.com>
2022-11-15 10:03:15 +0300

    created the topic model and message model

--- base/models.py
+++ base/models.py
@@ -3,9 +3,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __self__(self):
+        return self.name
+
+
 class Room(models.Model):
-    # host =
-    # topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participants =
@@ -17,7 +24,7 @@ class Room(models.Model):
 
 
 class Message(models.Model):
-    # user =
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
