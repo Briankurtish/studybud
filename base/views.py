801e089aa43fbed790347692c09c894e240460af
Cipher <67450280+Briankurtish@users.noreply.github.com>
2022-12-03 17:46:01 +0300

    where i ended

--- base/views.py
+++ base/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
+
 # Create your views here.
 
 # rooms = [
@@ -12,6 +16,23 @@ from .forms import RoomForm
 
 
 def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('create-room')
+            else:
+                messages.error(request, 'Username or Password does not exist')
+
     context = {}
     return render(request, 'base/login_register.html', context)
 
